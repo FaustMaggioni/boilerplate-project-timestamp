935daa11bd671b12122a0f6a43b3af68694c18f5
Faustino <faustino.maggioni@est.fi.uncoma.edu.ar>
2021-10-05 16:04:22 -0300

    error and trycatch added

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
 const moment = require('moment');
+
 const months = require('./months');
 const weekdays = require('./weekdays');
 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));
 
 app.use(express.static('public'));
@@ -19,6 +21,32 @@ app.get("/api/hello", function (req, res) {
 
 app.get("/api/:date", function (req,res) {
   const dateparam = req.params.date;
+
+  try {
+    const {date, unix} = getDateAndUnix(dateparam);
+    const weekday = date.getDay();
+    const monthday = date.getDate()+1
+    const monthnumber = date.getMonth();
+    const year = date.getFullYear();
+  
+    const utc = `${weekdays.get(weekday)}, ${monthday} ${months.get(monthnumber)} ${year} 00:00:00 GMT`;
+    
+    res.json({
+      unix: unix, 
+      utc: utc
+    })
+
+  } catch (error) {
+    res.json({error: error.message})
+  }
+
+})
+
+function isValidDate(date_string){
+  return moment(date_string, "YYYY-MM-DD", true).isValid();
+}
+
+function getDateAndUnix(dateparam){  
   let date;
   let unix;
 
@@ -26,33 +54,15 @@ app.get("/api/:date", function (req,res) {
     date = new Date(dateparam);
     unix = date.getTime();
   }else{
-
     if(isNaN(dateparam)){
-      return res.json({ error : "Invalid Date" });
+      throw new Error("Invalid date")
     }
-
-    unix = dateparam;
+    unix = parseFloat(dateparam);
     date = new Date(unix);
   }
-
-  const weekday = date.getDay();
-  const monthday = date.getDate()+1
-  const monthnumber = date.getMonth();
-  const year = date.getFullYear();
-
-  const utc = `${weekdays.get(weekday)}, ${monthday} ${months.get(monthnumber)} ${year} 00:00:00 GMT`;
-  
-  res.json({
-    unix: unix, 
-    utc:utc
-  })
-  
-})
-
-function isValidDate(date_string){
-  return moment(date_string, "YYYY-MM-DD", true).isValid();
+  return {date, unix}
 }
 
-var listener = app.listen(5000, function () {
+const listener = app.listen(5000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
